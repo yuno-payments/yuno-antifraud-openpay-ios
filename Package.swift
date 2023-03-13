5d11d5404182343dc06ec4742fff9f765dd02b9a
Lautaro Pinto <lautaro.pinto@y.uno>
2023-03-13 17:49:39 -0300

    Fix typo

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "YunoAntifraudOpenpay",
-            targets: ["YunoAntifraudOpenpay", "OpenpaKit"]),
+            targets: ["YunoAntifraudOpenpay", "OpenpayKit"]),
     ],
     targets: [
         .binaryTarget(
